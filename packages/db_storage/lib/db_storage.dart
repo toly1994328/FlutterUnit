cce33f4f6666664170cbcb78b961304a6b43868f
toly <1981462002@qq.com>
2022-09-30 18:37:06 +0800

    优化项目结构

---
+++ packages/db_storage/lib/db_storage.dart
@@ -0,0 +1,15 @@
+library db_storage;
+
+export 'src/model/enums.dart';
+export 'src/model/widget_filter.dart';
+
+export 'src/dao/category_dao.dart';
+export 'src/dao/like_dao.dart';
+export 'src/dao/node_dao.dart';
+export 'src/dao/widget_dao.dart';
+
+export 'src/bean/category_po.dart';
+export 'src/bean/node_po.dart';
+export 'src/bean/widget_po.dart';
+export 'src/local_db.dart';
+export 'src/db_open_helper.dart';
---
+++ packages/widget_repository/test/widget_repository_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:widget_repository/widget_repository.dart';
+
+void main() {
+
+}
