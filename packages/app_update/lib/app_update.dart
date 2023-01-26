a955c351bc14140c0f4cd2df15bebac23c6fd920
toly <1981462002@qq.com>
2023-01-26 19:52:13 +0800

    优化代码结构

---
+++ packages/app_update/lib/app_update.dart
@@ -0,0 +1,8 @@
+library app_update;
+
+export 'bloc/bloc.dart';
+export 'bloc/state.dart';
+export 'bloc/event.dart';
+export 'model/app_info.dart';
+export 'views/app_update_panel.dart';
+export 'views/update_red_point.dart';
