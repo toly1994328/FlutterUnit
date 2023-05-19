0c99a5a369d7cd0e9730bba4b1e99a3949395f82
toly <1981462002@qq.com>
2023-05-19 17:31:51 +0800

    优化存储访问

--- packages/app_config/lib/repository/app_state_repository.dart
+++ packages/app_config/lib/repository/app_state_repository.dart
@@ -22,7 +22,7 @@ class AppStateRepository{
     //数据库不存在，执行拷贝
     String databasesPath = await DbOpenHelper.getDbDirPath();
     String dbPath = path.join(databasesPath, "flutter.db");
-    SpStorage.instance.initSp();
+    await SpStorage.instance.initSp();
     // 读取配置文件，初始化应用状态
     AppConfigPo po = await sp.appConfig.read();
 
