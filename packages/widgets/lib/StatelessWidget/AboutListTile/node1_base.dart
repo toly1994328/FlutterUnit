8fd4103289ee768864d3a1a206991209b18093a6
toly <1981462002@qq.com>
2023-10-24 07:48:49 +0800

    change_notifier_01

--- packages/widgets/lib/StatelessWidget/AboutListTile/node1_base.dart
+++ packages/widgets/lib/StatelessWidget/AboutListTile/node1_base.dart
@@ -1,8 +1,9 @@
 
+import 'package:flutter/material.dart';
+
 /// create by 张风捷特烈 on 2020/4/25
 /// contact me by email 1981462002@qq.com
-/// 说明: 
-
+/// 说明:
 //    {
 //      "widgetId": 193,
 //      "name": 'AboutListTile基本使用',
@@ -15,8 +16,6 @@
 //          "【applicationLegalese】 : 应用律术   【String】\n"
 //          "【aboutBoxChildren】 : 弹框内容组件   【List<Widget>】",
 //    }
-
-import 'package:flutter/material.dart';
 
 class AboutListTileDemo extends StatelessWidget {
   const AboutListTileDemo({Key? key}) : super(key: key);
@@ -34,7 +33,7 @@ class AboutListTileDemo extends StatelessWidget {
           padding: EdgeInsets.all(10.0),
           child: Text(
             '      FlutterUnit是【张风捷特烈】的开源项目，'
-                '收录Flutter的200+组件，并附加详细介绍以及操作交互，'
+                '收录Flutter的300+组件，并附加详细介绍以及操作交互，'
                 '希望帮助广大编程爱好者入门Flutter。'
                 '更多知识可以关注掘金账号、公众号【编程之王】。',
             style: TextStyle(color: Color(0xff999999), fontSize: 16),
