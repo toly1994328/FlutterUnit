1aaa3cd83687822d499036fec0a2bf844feade07
toly <1981462002@qq.com>
2024-01-18 15:07:35 +0800

    添加DropdownMenu

---
+++ packages/widgets/lib/StatefulWidget/DropdownMenu/node2.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+//    {
+//      "widgetId": 370,
+//      "name": '下拉菜单样式设置',
+//      "priority": 2,
+//      "subtitle":
+//          "【controller】 : 文字输入控制器   【TextEditingController?】\n"
+//          "【label】 : 输入框标签   【Widget?】\n"
+//          "【textStyle】 : 输入框文字样式   【TextStyle?】\n"
+//          "【inputDecorationTheme】 : 输入框装饰主题   【InputDecorationTheme?】\n"
+//          "【leadingIcon】 : 左侧图标   【Widget?】\n"
+//          "【trailingIcon】 : 右侧为展开菜单时图标   【Widget?】\n"
+//          "【selectedTrailingIcon】 : 右侧展开菜单时图标   【Widget?】\n"
+//          "【hintText】 : 输入框提示文字   【String?】\n"
+//          "【helperText】 : 输入框辅助文字   【String?】\n"
+//          "【errorText】 : 输入框错误文字   【String?】\n"
+//          "【menuStyle】 : 弹出菜单样式   【MenuStyle?】",
+//    }
+enum ColorLabel {
+  blue('Blue', Colors.blue),
+  pink('Pink', Colors.pink),
+  green('Green', Colors.green),
+  yellow('Orange', Colors.orange),
+  grey('Grey', Colors.grey);
+
+  const ColorLabel(this.label, this.color);
+
+  final String label;
+  final Color color;
+}
+
+
+enum IconLabel {
+  smile('Smile', Icons.sentiment_satisfied_outlined),
+  cloud('Cloud', Icons.cloud_outlined),
+  brush('Brush', Icons.brush_outlined),
+  heart('Heart', Icons.favorite);
+
+  const IconLabel(this.label, this.icon);
+
+  final String label;
+  final IconData icon;
+}
+
+class DropdownMenuNode2 extends StatefulWidget {
+  const DropdownMenuNode2({super.key});
+
+  @override
+  State<DropdownMenuNode2> createState() => _DropdownMenuNode2State();
+}
+
+class _DropdownMenuNode2State extends State<DropdownMenuNode2> {
+  final TextEditingController colorController = TextEditingController();
+  final TextEditingController iconController = TextEditingController();
+  ColorLabel? selectedColor;
+  IconLabel? selectedIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildColorMenu(),
+              const SizedBox(width: 24),
+              _buildLabelMenu(),
+            ],
+          ),
+        ),
+        _buildInfo()
+      ],
+    );
+  }
+
+  Widget _buildColorMenu(){
+    return DropdownMenu<ColorLabel>(
+      initialSelection: ColorLabel.green,
+      controller: colorController,
+      requestFocusOnTap: true,
+      label: const Text('Color'),
+      width: 250,
+      menuHeight: 150,
+      menuStyle: const MenuStyle(
+        backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+        surfaceTintColor: MaterialStatePropertyAll<Color>(Colors.white),
+        padding: MaterialStatePropertyAll<EdgeInsetsGeometry>(EdgeInsets.symmetric(vertical: 20,horizontal: 8)),
+      ),
+      onSelected: (ColorLabel? color) {
+        setState(() {
+          selectedColor = color;
+        });
+      },
+      dropdownMenuEntries: ColorLabel.values.map((ColorLabel color) {
+            return DropdownMenuEntry<ColorLabel>(
+              value: color,
+              label: color.label,
+              enabled: color.label != 'Grey',
+              style: MenuItemButton.styleFrom(
+                foregroundColor: color.color,
+              ),
+            );
+          }
+      ).toList(),
+    );
+  }
+
+  Widget _buildInfo() {
+    if (selectedColor != null && selectedIcon != null) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('You selected a ${selectedColor?.label} ${selectedIcon?.label}'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Icon(
+              selectedIcon?.icon,
+              color: selectedColor?.color,
+            ),
+          )
+        ],
+      );
+    }
+    return const Text('Please select a color and an icon.');
+  }
+
+  Widget _buildLabelMenu() {
+    return DropdownMenu<IconLabel>(
+      controller: iconController,
+      enableFilter: false,
+      requestFocusOnTap: false,
+      leadingIcon: const Icon(Icons.search),
+      label: const Text('Icon'),
+      inputDecorationTheme: const InputDecorationTheme(
+        filled: true,
+        contentPadding: EdgeInsets.symmetric(vertical: 5.0),
+      ),
+      onSelected: (IconLabel? icon) {
+        setState(() {
+          selectedIcon = icon;
+        });
+      },
+      dropdownMenuEntries: IconLabel.values.map((IconLabel icon) {
+          return DropdownMenuEntry<IconLabel>(
+            value: icon,
+            label: icon.label,
+            leadingIcon: Icon(icon.icon),
+          );
+        },
+      ).toList(),
+    );
+  }
+}
+
+
+
---
+++ packages/widgets/lib/StatefulWidget/DropdownMenu/node3.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+//    {
+//      "widgetId": 370,
+//      "name": '下拉菜单自定义菜单项',
+//      "priority": 3,
+//      "subtitle":
+//          "可以通过 DropdownMenuEntry 的 labelWidget 定制构建菜单项。",
+//    }
+class User {
+  final String name;
+  final bool man;
+  final String image;
+
+  const User(this.name, this.man, this.image);
+
+  @override
+  String toString() {
+    return 'User{name: $name, man: $man, image: $image}';
+  }
+}
+
+class DropdownMenuNode3 extends StatefulWidget {
+  const DropdownMenuNode3({ super.key });
+
+  @override
+  State<DropdownMenuNode3> createState() => _DropdownMenuNode3State();
+}
+
+class _DropdownMenuNode3State extends State<DropdownMenuNode3> {
+  late final TextEditingController controller;
+
+   List<User> data = const [
+    User('toly', true, 'icon_5.webp'),
+    User('toly49', false, 'icon_6.webp'),
+    User('toly42', true, 'icon_7.webp'),
+    User('toly56', false, 'icon_8.webp'),
+    User('card', true, 'icon_5.webp'),
+    User('ls', true, 'icon_6.webp'),
+    User('alex', true, 'icon_7.webp'),
+    User('fan sha', false, 'icon_8.webp'),
+  ];
+
+  User? _selectUser;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Created by Google Bard from 'create a lyrical phrase of about 25 words that begins with "is a color"'.
+    const String longText = 'is a color that sings of hope, A hue that shines like gold. It is the color of dreams, A shade that never grows old.';
+
+    return Scaffold(
+      body: Center(
+        child: DropdownMenu<User>(
+          width: 300,
+          menuHeight: 250,
+          controller: controller,
+          leadingIcon:  _selectUser!=null?SizedBox(
+            width: 26,
+            height: 26,
+            child: Center(
+              child: CircleAvatar(
+                radius: 14,
+                foregroundColor: Colors.transparent,
+                backgroundImage:
+                AssetImage('assets/images/head_icon/${_selectUser!.image}'),
+              ),
+            ),
+          ):null,
+          label: const Text('选择用户'),
+          onSelected: (User? user) {
+            print('Selected $user');
+            setState(() {
+              _selectUser = user;
+
+            });
+          },
+          dropdownMenuEntries: data.map<DropdownMenuEntry<User>>((User user) {
+            final String labelText = '${user.name} $longText\n';
+            return DropdownMenuEntry<User>(
+              value: user,
+              label: user.name,
+              // Try commenting the labelWidget out or changing
+              // the labelWidget's Text parameters.
+              labelWidget: _UserItem(user: user),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+class _UserItem extends StatelessWidget {
+  final User user;
+
+  const _UserItem({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 6),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            foregroundColor: Colors.transparent,
+            backgroundImage:
+            AssetImage('assets/images/head_icon/${user.image}'),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(user.name),
+              Text(
+                '性别: ${user.man ? '男' : '女'}',
+                style: const TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
